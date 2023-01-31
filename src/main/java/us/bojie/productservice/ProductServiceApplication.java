5b2befb53460cd992ad53a8023fb049199099468
Bojie Jiang <jiangbojie@gmail.com>
2023-01-30 22:41:38 -0800

    Init

---
+++ src/main/java/us/bojie/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package us.bojie.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
